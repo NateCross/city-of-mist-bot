ba542b82b9c05540c0d6eb53c0dd511c64c8623a
NateCross <jonathan.a.b.cross@gmail.com>
2022-02-18 14:29:34 +0800

    Init work on btns; need to pass func to btn

--- commands/themes.js
+++ commands/themes.js
@@ -3,9 +3,50 @@ const { MessageActionRow, MessageButton, MessageSelectMenu, MessageEmbed } =
   require('discord.js');
 const fs = require('fs');
 
-// class ButtonTemplate {
-//   
+// Declaring buttons to be used
+
+// class ThemeButton {
+//   constructor(id, label) {
+//     this.button = new MessageButton()
+//       .setCustomId(id)
+//       .setLabel(label)
+//       .setStyle('PRIMARY');
+//   }
 // }
+
+const InfoButton = {
+  button: new MessageButton()
+    .setCustomId('info')
+    .setLabel('Info')
+    .setStyle('PRIMARY'),
+  execute: (themeObj, embed) => {
+    console.log(Object.keys(themeObj.power));
+    embed.setTitle('Power Tags');
+
+    embed.fields = [];
+
+    Object.keys(themeObj.power).forEach(key => embed.addFields(
+      { name: key, value: themeObj.power[key] }
+    ));
+  }
+};
+
+// InfoButton.button.execute = (themeObj, embed) => {
+//     console.log(Object.keys(themeObj.power));
+//     embed.setTitle('Power Tags');
+//
+//     embed.fields = [];
+//     Object.keys(themeObj.power).forEach(key => embed.addFields(
+//       { name: key, value: themeObj.power[key] }
+//     ));
+// };
+
+const buttonList = [ InfoButton ];
+const buttonRow = new MessageActionRow();
+buttonList.forEach(key => buttonRow.addComponents(key.button));
+
+let baseEmbed = new MessageEmbed()
+  .setColor('#E300D2')
 
 const filenames = fs.readdirSync('./assets/themes')
                   .filter(n => n.endsWith('.json'));
@@ -20,60 +61,79 @@ const listOfTypes = themeList.map(theme => theme.type);
 
 console.log(listOfTypes);
 
-const selectMenuOptions = themeList.map(theme => ({
-  label: theme.name,
-  // description: "",
-  value: theme.id,
-}));
+const selectMenuOptions = {};
+listOfTypes.forEach(type =>
+  selectMenuOptions[type] = themeList
+    .filter(theme => theme.type === type)
+    .map(theme => ({
+      label: theme.name,
+      value: theme.id,
+    }))
+)
 
-console.log(selectMenuOptions);
+// console.log(selectMenuOptions);
 
 const selectMenu = new MessageActionRow()
   .addComponents(
     new MessageSelectMenu()
       .setCustomId('theme-name')
       .setPlaceholder('Select a theme to view')
-      .addOptions(selectMenuOptions),
+      // .addOptions(selectMenuOptions),
   );
 
 module.exports = {
+  // Setting up command info
   data: new SlashCommandBuilder()
     .setName('themes')
     .setDescription('Looks up one of the themes.')
-    .addStringOption(option =>
+    .addStringOption(option => {
       option.setName('type')
         .setDescription('Type of theme to lookup')
-        .setRequired(true)
-        .addChoice('Mythos', 'Mythos')
-        .addChoice('Logos', 'Logos')
-    ),
+        .setRequired(true);
+      listOfTypes.forEach(type => option.addChoice(type, type));
+      return option;
+    }),
+
+  //////////////////////////////
+  /// Main driver of command ///
+  //////////////////////////////
   async execute(interaction) {
+    let selectedMenuItem;
     console.log(interaction.options.get("type").value);
     const option = interaction.options.get("type").value;
-    const filteredMenu = selectMenu.filter(theme =>
-      theme.type === option
-    )
+
+    selectMenu.components[0].addOptions(selectMenuOptions[option]);
+
     await interaction.reply({
       content: `You selected: ${option}`,
-      components: [filteredMenu],
+      components: [selectMenu],
     });
 
-    // const filter = i => moveList.some(button => {
-    //   return i.customId === button.id;
-    // });
     const collector = interaction.channel.createMessageComponentCollector({ time: 100000 });
 
-    collector.on('collect', i => {
+    collector.on('collect', async i => {
       if (i.isSelectMenu()) {
         console.log(`interaction: ${i.values[0]}`);
-        const selectedMenuItem = themeList.find(theme =>
+        selectedMenuItem = themeList.find(theme =>
           theme.id === i.values[0]
         );
         console.log(selectedMenuItem);
-        const updatedEmbed = new MessageEmbed()
-          .setColor('#E300D2')
+        const initialEmbed = baseEmbed
           .setTitle(selectedMenuItem.name)
+          .setDescription(selectedMenuItem.concept);
           // TODO: Fill in the basic info field as description
+
+        // InfoButton.execute(selectedMenuItem, initialEmbed);
+        // const selectMenuAndButtons = [selectMenu].concat(buttonList);
+        await i.update({
+          embeds: [initialEmbed],
+          components: [selectMenu, buttonRow],
+        });
+
+      } else if (i.isButton()) {
+        console.log('You pressed a button');
+        console.log(i);
+        const updatedEmbed = baseEmbed;
       }
     });
 
